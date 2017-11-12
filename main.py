905d08838130c99ccfa01d30752a15f5c26f0c71
Jeremiah Greer <jeremiahgreer@gmail.com>
2017-11-12 10:16:14 -0500

    Added story importance

--- main.py
+++ main.py
@@ -3,17 +3,37 @@ import file_parse as fp
 import story_importance as si
 
 
+
+
+usage = """Usage:   python main.py <userStoryFileName.txt> <filterWord> <resultsFileName>
+  <userStoryFileName.txt> is the name of the user story file to be read.
+  <filterWord> is the word used to show user stories pertinent to that word.
+  <resultsFileName> is the name of the result file."""
+
+
 def main():
   fName = ""
+  filterWord = ""
   
   # Attempt to read argument
   try:
     fName = sys.argv[1]
   except:
     print("Error: No filename given")
+    print(usage)
     return
     
+  try:
+    filterWord = sys.argv[2]
+  except:
+    filterWord = ""
+    
   userStories = fp.parseFile(fName)
+  if (filterWord != ""):
+    userStories = si.getRelevantStories(userStories, "Workgroup")
+    
+  
+    
   
   
   
---
+++ story_importance.py
@@ -0,0 +1,23 @@
+import file_parse as fp
+
+
+# Returns a list of relevant user stories based on the relevant string
+# String is assumed to be one word for this assignment
+def getRelevantStories(list_of_stories, relevantString):
+  
+  # Normalize string using same mechanism as read-in lines
+  normString = fp.preProcessStory(fp.preProcessLine(relevantString))
+  relevantStories = []
+  
+  # Find all user stories relevant by checking if string is contained
+  # Basic method which can be expanded upon later for other uses
+  for story in list_of_stories:
+    wordList = story.split(" ")
+    #print(wordList)
+    for word in wordList:
+      if (word.find(normString) != -1):
+        relevantStories.append(story)
+        break;
+        
+  return relevantStories
+    
