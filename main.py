6cc97e292844234e7c425f4dfa44c63f97e4e286
Jeremiah Greer <jeremiahgreer@gmail.com>
2017-11-12 08:27:23 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+
+def main():
+  fName = ""
+  try:
+    fName = sys.argv[1]
+  except:
+    print("Error: No Filename given/found")
+    return
+    
+  
+  
+  
+  
+  
+  
+  
+if __name__ == "__main__": 
+  main()
