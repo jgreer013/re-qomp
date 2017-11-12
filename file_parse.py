980dc1fe12234882f636e38ece35e41c294ff3a0
Jeremiah Greer <jeremiahgreer@gmail.com>
2017-11-12 09:01:52 -0500

    Added comments to current files

--- file_parse.py
+++ file_parse.py
@@ -1,16 +1,17 @@
 
 
 
-
+# Function to parse user story file
+# Input: User Story Filename as a string (i.e. "UCRATE-47-USs.txt")
 def parseFile(filename):
   f = open(filename, 'r')
   stories = []
   
   prevLine = "stop"
   currStory = ""
+  
+  # Iterate through all lines in the file
   for line in f:
-    #print("Line:", line)
-    
     if ((line == '\r\n' and prevLine == '\r\n') or 
         (line == '\n' and prevLine == '\n')):
       currStory = preProcessStory(currStory)
@@ -24,6 +25,8 @@ def parseFile(filename):
     
     prevLine = line
     
+    
+  # Add the last user story (due to file not ending at the correct location)
   currStory = preProcessStory(currStory)
   stories.append(currStory)
     
@@ -31,11 +34,12 @@ def parseFile(filename):
   
   
   
-  
+# Remove trailing whitespace and change everything to lowercase
 def preProcessStory(story):
   story = story.strip().lower()
   return story
   
+# Remove newlines, returns (windows why), and remove colons
 def preProcessLine(line):
   line = line.strip('\n').strip('\r').replace(":", '')
   return line
--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import story_importance as si
 
 def main():
   fName = ""
+  
+  # Attempt to read argument
   try:
     fName = sys.argv[1]
   except:
-    print("Error: No Filename given/found")
+    print("Error: No filename given")
     return
     
   userStories = fp.parseFile(fName)
